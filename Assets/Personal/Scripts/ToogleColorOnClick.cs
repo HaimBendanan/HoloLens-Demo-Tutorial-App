185457656f3ebf234f721f9fb5330287517b3054
Haim Bendanan <HaimBendanan@gmail.com>
2017-06-27 23:52:24 +0300

    Initial Code
    First check in

---
+++ Assets/Personal/Scripts/ToogleColorOnClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using Random = UnityEngine.Random;
+
+public class ToogleColorOnClick : MonoBehaviour, IInputClickHandler
+{
+    private MeshRenderer _meshRenderer;
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        SetCubeColor();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        SetCubeColor();
+    }
+
+    private void SetCubeColor()
+    {
+        _meshRenderer.material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+    }
+}
